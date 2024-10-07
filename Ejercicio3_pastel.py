81a71fa503368d2b8ad7c4af6bba451fe319e416
EduUgb <carloseduardog3200@gmail.com>
2024-10-06 21:02:22 -0600

    correccion en la salida

--- Ejercicio3_pastel.py
+++ Ejercicio3_pastel.py
@@ -8,6 +8,7 @@ df = pd.read_csv("CARS_1.csv")
 
 #Este es un limitante para que solo nos permita dar 10 resultados
 topN = 10
+
 dfTop = df.nlargest(topN, 'reviews_count')
 
 #Aquí escogemos las columnas que usaremos para extraer sus datos
@@ -16,6 +17,8 @@ autos = dfTop['car_name']
 colores = ['#FF0000', '#32CD32', '#FF00FF', '#1ABC9C', '#F8E231', '#FFC67D', '#0097A7', '#C7B8EA', '#FFD7BE', '#4B0082']
 
 #Creamos el gráfico circular o de pastel asignandole sus titulos respectivos
-plt.pie(frec, labels = autos, autopct='%d', colors=colores)
+plt.pie(frec, labels=autos, autopct=lambda p: f'{int(p * sum(frec) / 100)}', colors=colores)
 plt.title("Número de reseñas de autos")
 plt.show()
+
+
