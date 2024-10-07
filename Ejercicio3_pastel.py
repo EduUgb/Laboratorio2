df0ed30783535301038c50248fa826125bf37e07
Ar1510 <arielst299@gmail.com>
2024-10-06 20:37:51 -0600

    Correccion

--- Ejercicio3_pastel.py
+++ Ejercicio3_pastel.py
@@ -8,11 +8,11 @@ df = pd.read_csv("CARS_1.csv")
 
 #Este es un limitante para que solo nos permita dar 10 resultados
 topN = 10
-dfTop = df.nlargest(topN, 'recuento de reseñas')
+dfTop = df.nlargest(topN, 'reviews_count')
 
 #Aquí escogemos las columnas que usaremos para extraer sus datos
-frec = dfTop['recuento de reseñas']
-autos = dfTop['nombre_del_coche']
+frec = dfTop['reviews_count']
+autos = dfTop['car_name']
 colores = ['#FF0000', '#32CD32', '#FF00F', '#1ABC9C', '#F8E231', '#FFC67D', '#0097A7', '#C7B8EA', '#FFD7BE', '#4B0082']
 
 #Creamos el gráfico circular o de pastel asignandole sus titulos respectivos
